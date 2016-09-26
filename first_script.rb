80ce3f5a6398f995b0f7953001b074bbb066d822
aditya7iyengar <adi.iyengar@annkissam.com>
2016-09-26 12:02:07 -0400

    Update script

--- first_script.rb
+++ first_script.rb
@@ -1,23 +1,59 @@
 require "Sphero"
+require 'io/console'
 
-Sphero.start '/dev/tty.Sphero-WRO-AMP-SPP' do
-    # Roll 0 degrees, speed 125
-    x = 0
-    while x < 30 do
-        roll 500, 60
-
-        # # Turn 360 degrees, 30 degrees at a time
-        # 0.step(360, 30) { |h|
-        # h = 0 if h == 360
-
-        #     # Set the heading to h degrees
-        #     heading = h
-        #     keep_going 1
-        # }
-
-        keep_going 1
-
-        x += 30
-        # stop
-    end
+def roll_right
+  roll 25, 270
 end
+
+def roll_left
+  roll 25, 90
+end
+
+def roll_front
+  roll 25, 0
+end
+
+def roll_back
+  roll 25, 180
+end
+
+def read_char
+  STDIN.echo = false
+  STDIN.raw!
+
+  input = STDIN.getc.chr
+  if input == "\e" then
+    input << STDIN.read_nonblock(3) rescue nil
+    input << STDIN.read_nonblock(2) rescue nil
+  end
+ensure
+  STDIN.echo = true
+  STDIN.cooked!
+
+  return input
+end
+
+def command_sphero
+  char = read_char
+
+  Sphero.start '/dev/tty.Sphero-WRO-AMP-SPP' do
+    case char
+    when "\e[A"
+      #puts "UP ARROW"
+      roll_front
+    when "\e[B"
+      #puts "DOWN ARROW"
+      roll_back
+    when "\e[C"
+      #puts "RIGHT ARROW"
+      roll_right
+    when "\e[D"
+      #puts "LEFT ARROW"
+      roll_left
+    else
+      puts "NO COMMAND REGSTERED FOR: #{c.inspect}"
+    end
+  end
+end
+
+command_sphero while(true)
